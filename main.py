f31eda0e2bcda5793d39bca76320f489107951b2
kolodziejj <oliwiako94@gmail.com>
2024-12-17 13:30:51 +0100

    player moving

--- main.py
+++ main.py
@@ -11,18 +11,29 @@ def main():
 )
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
+
 
 
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                  return
-        screen.fill("#000000")
-        player.draw(screen)
-        pygame.display.flip()
 
+        for obj in updatable:
+            obj.update(dt) 
+        
+        screen.fill("#000000")
+        
+        for obj in drawable:
+            obj.draw(screen)
+
+        pygame.display.flip()
+        
         # limit the framerate to 60 FPS
         dt = clock.tick(60)/1000
         
