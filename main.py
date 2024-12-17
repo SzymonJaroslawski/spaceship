b5340331ed2b74f9beba4b1bba001d11110672e4
kolodziejj <oliwiako94@gmail.com>
2024-12-17 12:26:07 +0100

    limit FPS to 60, add circleshape.py

--- main.py
+++ main.py
@@ -9,12 +9,20 @@ def main():
         f"Screen height: {SCREEN_HEIGHT}"
 )
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                  return
         screen.fill("#000000")
         pygame.display.flip()
+        
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60)/1000
+
+
 
 if __name__ == "__main__":
     main()
